330973a73ab0c02ae3472f48c3dce87015a493bc
Aditya Raj <adityasharma14807@gmail.com>
2026-07-31 21:48:10 +0530

    Message Router setup

--- server/server.js
+++ server/server.js
@@ -4,13 +4,11 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 // Create Express app and HTTP server
 const app = express();
 const server = http.createServer(app);
-
-// Connect Database
-connectDB();
 
 // Middleware
 app.use(express.json({ limit: "4mb" }));
@@ -18,14 +16,17 @@ app.use(cors());
 
 // Test Route
 app.get("/api/status", (req, res) => {
-    res.send("Server is live");
+  res.send("Server is live");
 });
 
 //Routes setup
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
+// Connect Database
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-    console.log(`🚀 Server is running on PORT: ${PORT}`);
+  console.log(`🚀 Server is running on PORT: ${PORT}`);
 });
